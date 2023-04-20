99ee05a6247e787caea0a59a8dec2e0f7e8f59d9
alberto-paparella <js.flakes46@gmail.com>
2023-04-20 14:45:09 +0200

    Added LogisticRegressor; modularized experiment function

--- sequoia_comparison/IJECE.py
+++ sequoia_comparison/IJECE.py
@@ -6,7 +6,7 @@ if not path in sys.path:
 del path
 
 from dataset.normalizer import csv_importer_full
-from sequoia_comparison.utils import find_demarcator, shuffle_and_split, get_custom_dataset, get_scores
+from sequoia_comparison.utils import find_demarcator, experiment
 
 LOOPS = 500
 
@@ -14,42 +14,8 @@ default_dataset = csv_importer_full("dataset/sources/user_fake_authentic_2class.
 idx = find_demarcator(default_dataset)
 
 fake = default_dataset[:idx]
-correct = default_dataset[idx:]
+real = default_dataset[idx:]
 
-### EXPERIMENT ###
-avg_scores = {
-    'default': {'precision': 0, 'accuracy': 0},
-    'custom': {'precision': 0, 'accuracy': 0}
-}
-
-print(f"Calculating precision and accuracy {LOOPS} times")
-
-for i in range(LOOPS):
-    # Get new train_df and validation_df, same for default and custom
-    train_df, validation_df = shuffle_and_split(fake, correct)
-    custom_train_df, custom_validation_df = get_custom_dataset(train_df, validation_df)
-
-    # Default scores
-    scores = get_scores(train_df, validation_df)
-    avg_scores['default']['precision'] += scores['precision']
-    avg_scores['default']['accuracy'] += scores['accuracy']
-
-    # Custom scores
-    scores = get_scores(custom_train_df, custom_validation_df)
-    avg_scores['custom']['precision'] += scores['precision']
-    avg_scores['custom']['accuracy'] += scores['accuracy']
-
-    print(f"{i+1}/{LOOPS}", end="\r")
-
-# Averaging
-for t in avg_scores.keys():
-    for s in avg_scores[t].keys():
-        avg_scores[t][s] /= LOOPS
-
-print('Done!\n\n')
-
-print('default avg precision:', "{:.3f}".format(avg_scores['default']['precision']))
-print('default avg accuracy:', "{:.3f}".format(avg_scores['default']['accuracy']))
-
-print('custom avg precision:', "{:.3f}".format(avg_scores['custom']['precision']))
-print('custom avg accuracy:', "{:.3f}".format(avg_scores['custom']['accuracy']))
+# Experiments
+experiment(fake, real, "dt", 20)   # DecisionTree
+experiment(fake, real, "lr", 20)   # LogisticRegression
--- sequoia_comparison/utils.py
+++ sequoia_comparison/utils.py
@@ -1,6 +1,7 @@
 import random
 import pandas as pd
 from sklearn import tree, metrics
+from sklearn.linear_model import LogisticRegression
 
 PERCENT_TRAIN = 70
 
@@ -76,12 +77,10 @@ def get_custom_dataset(train_df, validation_df, csv=False):
     return custom_train_df, custom_validation_df
 
 
-def get_scores(train_df, validation_df, csv=False):
+def get_scores(y_val, y_pred):
     scores = {
         'precision': 0, 'accuracy': 0
     }
-    y_val, y_pred = fit_decision_tree(train_df.iloc[:, :-2], train_df.iloc[:, -1], validation_df)
-
     scores['accuracy'] += metrics.accuracy_score(y_val, y_pred)
     scores['precision'] += metrics.precision_score(y_val, y_pred)
     return scores
@@ -91,3 +90,81 @@ def print_scores(train_df, validation_df, csv=False):
     scores = get_scores(train_df, validation_df, csv)
     print('precision:', "{:.3f}".format(scores['precision']))
     print('accuracy:', "{:.3f}".format(scores['accuracy']))
+
+'''
+modes:
+ - "dt" => DecisionTree
+ - "lr" => LogisticRegression
+'''
+def experiment(fake, correct, csv=False, mode="dt", n_iter=20):
+    avg_scores = {
+        'default': {'precision': 0, 'accuracy': 0},
+        'custom': {'precision': 0, 'accuracy': 0}
+    }
+
+    if mode == "dt":
+        print(f"Calculating precision and accuracy metrics for Decision Trees over {n_iter} times")
+    elif mode == "lr":
+        print(f"Calculating precision and accuracy metrics for Decision Trees over {n_iter} times")
+    else:
+        return -1
+
+    for i in range(n_iter):
+        # Get new train_df and validation_df, same for default and custom
+        train_df, validation_df = shuffle_and_split(fake, correct)
+        custom_train_df, custom_validation_df = get_custom_dataset(train_df, validation_df, csv)
+
+        # Default mode
+        if mode == "dt":
+            # Get new Decision Tree
+            clf = tree.DecisionTreeClassifier()
+            clf = clf.fit(train_df.iloc[:, :-2], train_df.iloc[:, -1])
+        elif mode == "lr":
+            # Get new Logistic Regressor
+            clf = LogisticRegression(random_state=0, max_iter=5000)
+            clf = clf.fit(train_df.iloc[:, :-2], train_df.iloc[:, -1])
+
+        # Get ground truth and predictions to measure performance
+        X_val, y_val = validation_df.iloc[:, :-2], validation_df.iloc[:, -1]
+        y_pred = clf.predict(X_val)
+
+        # Default scores
+        scores = get_scores(y_val, y_pred)
+        avg_scores['default']['precision'] += scores['precision']
+        avg_scores['default']['accuracy'] += scores['accuracy']
+
+        # Custom mode
+        if mode == "dt":
+            # Get new Decision Tree
+            clf = tree.DecisionTreeClassifier()
+            clf = clf.fit(custom_train_df.iloc[:, :-2], custom_train_df.iloc[:, -1])
+        elif mode == "lr":
+            # Get new Logistic Regressor
+            clf = LogisticRegression(random_state=0, max_iter=2500)
+            clf = clf.fit(custom_train_df.iloc[:, :-2], custom_train_df.iloc[:, -1])
+        else:
+            return -1
+        
+        # Get ground truth and predictions to measure performance
+        X_val, y_val = custom_validation_df.iloc[:, :-2], custom_validation_df.iloc[:, -1]
+        y_pred = clf.predict(X_val)
+
+        # Custom scores
+        scores = get_scores(y_val, y_pred)
+        avg_scores['custom']['precision'] += scores['precision']
+        avg_scores['custom']['accuracy'] += scores['accuracy']
+
+        print(f"{i+1}/{n_iter}", end="\r")
+
+    # Averaging
+    for t in avg_scores.keys():
+        for s in avg_scores[t].keys():
+            avg_scores[t][s] /= n_iter
+
+    print('Done!\n\n')
+
+    print('default avg precision:', "{:.3f}".format(avg_scores['default']['precision']))
+    print('default avg accuracy:', "{:.3f}".format(avg_scores['default']['accuracy']))
+
+    print('custom avg precision:', "{:.3f}".format(avg_scores['custom']['precision']))
+    print('custom avg accuracy:', "{:.3f}".format(avg_scores['custom']['accuracy']))
