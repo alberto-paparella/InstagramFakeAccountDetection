d12a0643633602735a062936f24624032054ee8a
Nemesis <lorenzo.balugani@gmail.com>
2023-06-26 18:08:16 +0200

    :sparkles: Add reduced size joined dataset

--- sequoia_comparison/combined.py
+++ sequoia_comparison/combined.py
@@ -1,6 +1,7 @@
 from dataset.normalizer import json_importer_full, csv_importer_full
 from dataset.utils import find_demarcator
 from utils.utils import experiment
+import random
 
 n_exp = 5
 
@@ -10,10 +11,27 @@ correct = json_importer_full("../dataset/sources/nonautomatedAccountData.json", 
 default_dataset = csv_importer_full("../dataset/sources/user_fake_authentic_2class.csv")
 idx = find_demarcator(default_dataset)
 
-fake = fake + default_dataset[:idx]
-correct = correct + default_dataset[idx:]
-# experiment(fake, correct, csv=False, mode="dt", n_iter=n_exp, combine=True)
-# experiment(fake, correct, csv=False, mode="lr", n_iter=n_exp, combine=True)
-# experiment(fake, correct, csv=False, mode="nb", n_iter=n_exp, combine=True)
-# experiment(fake, correct, csv=False, mode="rf", n_iter=n_exp, combine=True)
-experiment(fake, correct, csv=False, mode="dl", n_iter=n_exp, combine=True)
+experiments = ["dt", "lr", "nb", "rf", "dl"]
+
+dataset = {"full": dict(), "partial": dict()}
+
+dataset["full"]["fake"] = fake + default_dataset[:idx]
+dataset["full"]["correct"] = correct + default_dataset[idx:]
+d_d_f = default_dataset[:idx]
+random.shuffle(d_d_f)
+d_d_c = default_dataset[idx:]
+random.shuffle(d_d_c)
+dataset["partial"]["fake"] = fake + d_d_f[:700]
+dataset["partial"]["correct"] = correct + d_d_c[:700]
+
+print("\n================\n Running experiment on limited dataset... \n================\n")
+
+for exp in experiments:
+    experiment(dataset["partial"]["fake"], dataset["partial"]["correct"],
+               csv=False, mode=exp, n_iter=n_exp, combine=True)
+
+print("\n================\n Running experiment on full dataset... \n================\n")
+
+for exp in experiments:
+    experiment(dataset["full"]["fake"], dataset["full"]["correct"],
+               csv=False, mode=exp, n_iter=n_exp, combine=True)
